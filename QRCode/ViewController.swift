eec5ae642d1fccd878fb1368162e90ebeacef17f
Prateek <prat14k@IIMJobss-MacBook-Pro.local>
2017-09-30 20:52:45 +0530

    QR Code Generator added

--- QRCode/ViewController.swift
+++ QRCode/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var qrImage: UIImageView!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated);
+        
+        textField.becomeFirstResponder()
+    }
+    
+    @IBAction func generateAction(_ sender: UIButton) {
+        
+        if let inputStr = textField.text{
+            
+            let data = inputStr.data(using: .ascii, allowLossyConversion: false)
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            filter?.setValue(data, forKey: "inputMessage")
+            
+            let img = UIImage(ciImage: (filter?.outputImage)!)
+            
+           
+            qrImage.image = img
+            
+            
+        }
+        
+    }
+    
 }
 
